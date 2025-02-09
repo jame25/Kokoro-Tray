530fa4a16e9abeb1f85fb88def6e5501b9f5bbf0
jame25 <jame25@users.noreply.github.com>
2025-02-09 23:17:38 +0000

    Only one instance of executable allowed

--- Program.cs
+++ Program.cs
@@ -1,25 +1,49 @@
 using System;
 using System.Windows.Forms;
+using System.Threading;
+using System.Security.Principal;
 
 namespace KokoroTray
 {
     internal static class Program
     {
+        private static Mutex mutex = null;
+        private const string MutexName = "KokoroTrayApplication_SingleInstance_Mutex";
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            
-            using (var trayApp = new TrayApplication())
+            bool createdNew;
+            mutex = new Mutex(true, MutexName, out createdNew);
+
+            if (!createdNew)
             {
-                Application.Run();
+                Logger.Info("Another instance is already running. Exiting.");
+                MessageBox.Show("Another instance of Kokoro Tray is already running.", "Kokoro Tray", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            try
+            {
+                // To customize application configuration such as set high DPI settings or default font,
+                // see https://aka.ms/applicationconfiguration.
+                ApplicationConfiguration.Initialize();
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                
+                using (var trayApp = new TrayApplication())
+                {
+                    Application.Run();
+                }
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+                mutex.Dispose();
             }
         }
     }
